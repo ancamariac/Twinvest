cabcd8ab9a78bf9679734db2aae08d85c21d1e45
bogdanmihai1 <33834374+bogdanmihai1@users.noreply.github.com>
2022-12-03 19:36:39 +0200

    model de NN simpla + exemplu utilizare

---
+++ exemple-cod/custominput.py
@@ -0,0 +1,20 @@
+import numpy as np
+import tensorflow as tf
+from transformers import BertTokenizer, TFBertModel
+from tensorflow import keras
+
+NN_model = keras.models.load_model('C:\\Users\\Bogdan\\Desktop\\FakeNews\\model')
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = TFBertModel.from_pretrained('bert-base-uncased')
+
+print("Your input:")
+text = input()
+input_ids = tf.constant(tokenizer.encode(text, max_length=512, truncation=True))[None, :]
+outputs = model(input_ids)
+#np_arr = np.array(outputs)
+final = np.array(outputs["pooler_output"]).reshape(768)
+X_final = np.array([final])
+val_pred = NN_model.predict(X_final)
+print("Probabilitatea ca afirmatia sa fie adevarata:", val_pred * 100, "%")
