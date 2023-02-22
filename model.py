0ea626f4810a90b9e0b5ce437c013cb4ca830cc6
ancamariac <ciuciuancamaria@gmail.com>
2023-02-22 23:04:58 +0200

    Training model and custom input

--- model.py
+++ model.py
@@ -2,6 +2,9 @@ import pickle
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from sklearn.model_selection import train_test_split
+import numpy as np
+import itertools 
+from tensorflow import keras
 import matplotlib.pyplot as plt
 
 # load dataset
@@ -9,29 +12,118 @@ import matplotlib.pyplot as plt
 # Y = labels
 with open('D:\\Workspace\\tweets-sentiment-analysis\\embeddings.pk', 'rb') as f:
     X, Y = pickle.load(f)
+
+Y_aux = []
+
+# transformam fiecare element din Y in one hot encoding
+for elem in Y:
+    aux = [0, 0, 0]
+    aux[elem] = 1
+    Y_aux.append(aux)
+    
+X = np.array(X)
+Y = np.array(Y_aux)
+
     
 # split into train, val, test
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
 X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.125, random_state=42) # 0.125 x 0.8 = 0.1
 
-# initialize the model
-model = Sequential()
+# grid search dict
+dct_grid_space = {
+    'layer1' : [
+        256,
+        128,
+        512
+    ],
+    'layer2' : [
+        256,
+        128,
+        512
+    ],
+    'layer3' : [
+        256,
+        128,
+        512
+    ],
+    'dropout1' : [
+        0.1,
+        0.2,
+        0.3
+    ],
+    'opt_class' : [
+        'sgd',
+        'adam'
+    ],
+    'lr' : [
+        0.001,
+        0.01,
+    ]
+}
 
-# add the layers
-model.add(Dense(15, input_shape=(768, ), activation='relu')) 
-model.add(Dropout(0.25)) 
-model.add(Dense(5, activation='relu'))
-model.add(Dropout(0.25))
-model.add(Dense(3, activation='softmax'))
+# generate all combinations for grid search
+grid_params = []
+grid_values = []
+for k in dct_grid_space:
+    grid_params.append(k)
+    grid_values.append(dct_grid_space[k])
+# se face produsul cu toate elementele din grid_values
+grid_combs = list(itertools.product(*grid_values))
 
-# compile, train and evaluate
-model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
-history = model.fit(X_train, Y_train, batch_size=512, epochs=20, validation_data=(X_val, Y_val))
+def create_model(l1, l2, l3, d, opt_name, lr):
+    # initialize the model
+    model = Sequential()
+
+    # add the layers
+    model.add(Dense(l1, input_shape=(768, ), activation='relu')) 
+    model.add(Dropout(d)) 
+    model.add(Dense(l2, activation='relu'))
+    model.add(Dropout(d))
+    model.add(Dense(l3, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+
+    if opt_name == 'adam':
+        opt = keras.optimizers.Adam(learning_rate=lr)
+    else:
+        opt = keras.optimizers.SGD(learning_rate=lr)
+
+    # compile, train and evaluate
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=opt)
+    
+    return model
+
+best_accuracy = 0
+best_combination = grid_combs[0]
+
+# pentru fiecare combinare cream cate un model ca sa vedem
+# care are scorul cel mai bun
+for combination in grid_combs:
+    model = create_model(combination[0], combination[1], combination[2],
+                         combination[3], combination[4], combination[5])
+    
+    history = model.fit(X_train, Y_train, batch_size=512, epochs=100, validation_data=(X_val, Y_val), verbose=0)
+    # compara rezultatele cu cele reale -> face metricile modelului(loss, acc)
+    scores = model.evaluate(X_test, Y_test)
+    # print(f'Score: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
+    
+    if scores[1] > best_accuracy:
+        best_accuracy = scores[1]
+        best_combination = combination
+  
+print(best_combination)
+
+# se creeaza modelul cu cea mai buna combinatie obtinuta din grid search
+model = create_model(best_combination[0], best_combination[1], best_combination[2],
+                     best_combination[3], best_combination[4], best_combination[5])
+
+history = model.fit(X_train, Y_train, batch_size=512, epochs=100, validation_data=(X_val, Y_val))
+# compara rezultatele cu cele reale -> face metricile modelului(loss, acc)
 scores = model.evaluate(X_test, Y_test)
 print(f'Score: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
 
 # save the model
-model.save('D:\\Workspace\\tweets-sentiment-analysis\\model')
+model.save('D:\\Workspace\\tweets-sentiment-analysis\\model.h5')
 
 # validation loss
 plt.clf()
