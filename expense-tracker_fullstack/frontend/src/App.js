3e21fcade66fb250ca60352e530747b7557580d9
ancamariac <ciuciuancamaria@gmail.com>
2023-03-18 17:00:10 +0200

    Changed bg color

--- expense-tracker_fullstack/frontend/src/App.js
+++ expense-tracker_fullstack/frontend/src/App.js
@@ -52,6 +52,7 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg});
   position: relative;
+  background: linear-gradient(180deg, #804fcb 0%, #4bb982 100%);
   main{
     flex: 1;
     background: rgba(252, 246, 249, 0.78);
