d31fb9d06f42e10507b15ac6ec2607837c3bbe08
ancamariac <ciuciuancamaria@gmail.com>
2023-03-31 13:40:02 +0300

    Removed guards

--- expense-tracker_fullstack/frontend/src/Guards/RootGuard.js
+++
@@ -1,8 +0,0 @@
-import AuthGuard from "./AuthGuard";
-import React from "react";
-
-const RootGuard = ({ children }) => {
-  return <AuthGuard>{children}</AuthGuard>;
-};
-
-export default RootGuard;
