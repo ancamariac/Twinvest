2b5e7a79519e83b866b26e213fc1938466db9ae5
ancamariac <ciuciuancamaria@gmail.com>
2023-04-01 19:43:04 +0300

    logout

--- expense-tracker_fullstack/frontend/src/Components/Navigation/Navigation.js
+++ expense-tracker_fullstack/frontend/src/Components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ function Navigation({active, setActive}) {
     
     function logout() {
         sessionStorage.clear();
-        window.location.href = '/';
+        window.location.href = '/login';
     }
 
     return (
