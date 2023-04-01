bee6396dac017380e4cb533fe618a08babe2f1fe
bogdanmihai1 <33834374+bogdanmihai1@users.noreply.github.com>
2023-04-01 21:52:49 +0300

    Add files via upload

--- expense-tracker_fullstack/frontend/src/Components/Tweet/Tweet.js
+++ expense-tracker_fullstack/frontend/src/Components/Tweet/Tweet.js
@@ -2,29 +2,26 @@ import React, { Component } from "react";
 
 import TweetCard from "react-tweet-card";
 
-class Tweet extends Component {
-    constructor(props) {
-    super(props);
-    this.state = {};
-}
-
-    render() {
-        return (
-            <TweetCard
-            author={{
-                name: "randy",
-                username: "randyfactory",
-                image:
-                "https://pbs.twimg.com/profile_images/1382083582752096262/xrx0PO8Z_400x400.jpg"
-            }}
-            tweet={`how was “philosopher” ever a job lmao like was socrates sippin wine on a balcony somewhere drunkenly slurring shit like “to find urself, think for urself” with a crowd cheering underneath him like fuck yes socrates another banger this man will not miss`}
-            time={new Date(2021, 2, 2, 21, 3)}
-            source="Twitter for iPhone"
-            permalink="https://twitter.com/randyfactory/status/1366841622495961091" // optional
-            fitInsideContainer
-            />
-        );
-    }
-}
+function Tweet (tweetData) {
+    return (
+      <div style={{'marginTop':'15px', 'marginLeft':'auto', 'marginRight':'auto', 'width':'80%'}}>
+        <TweetCard 
+          author={{
+            name: tweetData.name,
+            username: tweetData.username,
+            image:tweetData.profile_image,
+          }}
+          tweet={tweetData.tweet}
+          time={Date(tweetData.date)}
+          source={tweetData.source}
+          permalink={tweetData.tweet_link}
+          engagement={{  
+            likes:tweetData.likes,
+            retweets:tweetData.retweets,
+          }}
+        />
+      </div>
+    );
+  }
 
 export default Tweet;
