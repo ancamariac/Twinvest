a5737ebd3879e4ef6b12ec6bc01727b43a95e02e
ancamariac <ciuciuancamaria@gmail.com>
2023-04-01 14:29:19 +0300

    Scraping data from Twitter and adding the results in mongo

--- crawler.py
+++ crawler.py
@@ -3,6 +3,8 @@ import csv
 import tweepy as tw
 import os
 import pymongo
+import requests
+from io import BytesIO
 from dotenv import load_dotenv
 
 # load the environment variables
@@ -16,14 +18,14 @@ access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
 # set up the MongoDB connection
 client = pymongo.MongoClient(os.getenv("MONGO_URI"))
-db = client.mydatabase
+db = client.test
 collection = db.tweets
 
 # set the search keywords
 search_words = "#economy OR #stocks OR #cryptocurrency"
 
 # set the number of tweets to retrieve
-num_of_tweets = 20
+num_of_tweets = 1
 
 # authenticate the API credentials
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
@@ -33,16 +35,19 @@ api = tw.API(auth, wait_on_rate_limit=True)
 # search for the tweets and write them to a CSV file
 tweets = tw.Cursor(api.search_tweets, q=search_words, lang="en", tweet_mode="extended").items(num_of_tweets)
 for tweet in tweets:
+    
     tweet_data = {
         "date": tweet.created_at,
         "name": tweet.user.name,
         "username": tweet.user.screen_name,
+        "profile_image": tweet.user.profile_image_url_https,
         "followers": tweet.user.followers_count,
         "likes": tweet.favorite_count,
         "tweet": tweet.full_text,
         "source": tweet.source,
         "retweets": tweet.retweet_count,
         "hashtags": [tag['text'] for tag in tweet.entities['hashtags']],
-        "mentions": [user['screen_name'] for user in tweet.entities['user_mentions']]
+        "mentions": [user['screen_name'] for user in tweet.entities['user_mentions']],
+        "tweet_link": f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"
     }
     collection.insert_one(tweet_data)
