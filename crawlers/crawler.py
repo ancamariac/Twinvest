cf6312a86e4b9226287837191b8b7332ed4b5254
ancamariac <ciuciuancamaria@gmail.com>
2023-04-01 16:26:41 +0300

    Modified crawler

--- crawlers/crawler.py
+++ crawlers/crawler.py
@@ -19,19 +19,10 @@ access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
 # set up the MongoDB connection
 client = pymongo.MongoClient(os.getenv("MONGO_URI"))
-db = client.test
-collection = db.tweets
+db = client.twinvest
+collection = db.tweet
 
-# set the search keywords
-crypto = [word.lower() for word in crypto]
-economicTerms = [word.lower() for word in economicTerms]
-stocks = [word.lower() for word in stocks]
-
-search_words = crypto + economicTerms + stocks
-
-#search_words = ["Bitcoin", "Ethereum", "Cardano", "Tesla", "Apple", "Microsoft"]
-#print(search_words[1:200])
-#exit(0)
+search_words = stocks
 
 # set the number of tweets to retrieve
 num_of_tweets = 1
@@ -42,21 +33,25 @@ auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
 # search for the tweets and write them to a CSV file
-tweets = tw.Cursor(api.search_tweets, q=crypto, lang="en", tweet_mode="extended").items(num_of_tweets)
+tweets = tw.Cursor(api.search_tweets, q=search_words, lang="en", tweet_mode="extended").items(num_of_tweets)
 for tweet in tweets:
-    
-    tweet_data = {
-        "date": tweet.created_at,
-        "name": tweet.user.name,
-        "username": tweet.user.screen_name,
-        "profile_image": tweet.user.profile_image_url_https,
-        "followers": tweet.user.followers_count,
-        "likes": tweet.favorite_count,
-        "tweet": tweet.full_text,
-        "source": tweet.source,
-        "retweets": tweet.retweet_count,
-        "hashtags": [tag['text'] for tag in tweet.entities['hashtags']],
-        "mentions": [user['screen_name'] for user in tweet.entities['user_mentions']],
-        "tweet_link": f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"
-    }
-    collection.insert_one(tweet_data)
+    print(tweet.user.followers_count)
+    if (tweet.user.followers_count > 1000 or tweet.favorite_count > 300
+    or tweet.retweet_count > 100):
+        
+        tweet_data = {
+            "date": tweet.created_at,
+            "name": tweet.user.name,
+            "username": tweet.user.screen_name,
+            "profile_image": tweet.user.profile_image_url_https,
+            "followers": tweet.user.followers_count,
+            "likes": tweet.favorite_count,
+            "tweet": tweet.full_text,
+            "source": tweet.source,
+            "retweets": tweet.retweet_count,
+            "hashtags": [tag['text'] for tag in tweet.entities['hashtags']],
+            "mentions": [user['screen_name'] for user in tweet.entities['user_mentions']],
+            "tweet_link": f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"
+        }
+        
+        collection.insert_one(tweet_data)
