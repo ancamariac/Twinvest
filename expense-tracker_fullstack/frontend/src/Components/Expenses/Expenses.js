b8d6f36cb5686a33f76b2880a647496a9b18b62d
ancamariac <ciuciuancamaria@gmail.com>
2023-04-09 13:49:40 +0300

    Reorganised js and css for Expenses

--- expense-tracker_fullstack/frontend/src/Components/Expenses/Expenses.js
+++ expense-tracker_fullstack/frontend/src/Components/Expenses/Expenses.js
@@ -1,21 +1,24 @@
 import React, { useEffect } from 'react'
-import styled from 'styled-components'
 import { useGlobalContext } from '../../context/globalContext';
-import { InnerLayout } from '../../styles/Layouts';
 import IncomeItem from '../IncomeItem/IncomeItem';
 import ExpenseForm from './ExpenseForm';
+import './Expenses.style.scss';
 
 function Expenses() {
     const {expenses, getExpenses, deleteExpense, totalExpenses} = useGlobalContext()
 
     useEffect(() =>{
         getExpenses()
-    }, [])
+    })
     return (
-        <ExpenseStyled>
-            <InnerLayout>
-                <h1>Expenses</h1>
-                <h2 className="total-income">Total Expense: <span>${totalExpenses()}</span></h2>
+        <div className="container">
+            <div className="row">
+                <h1> Expenses </h1>
+            </div>    
+            <div className="row content-row">
+                <div class="top-expense">
+                    <h2 className="total-income">Total Expense: <span>${totalExpenses()}</span></h2>
+                </div>
                 <div className="income-content">
                     <div className="form-container">
                         <ExpenseForm />
@@ -39,39 +42,9 @@ function Expenses() {
                         })}
                     </div>
                 </div>
-            </InnerLayout>
-        </ExpenseStyled>
+            </div>.
+        </div>
     )
 }
-
-const ExpenseStyled = styled.div`
-    display: flex;
-    overflow: auto;
-    .total-income{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background: #FCF6F9;
-        border: 2px solid #FFFFFF;
-        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
-        border-radius: 20px;
-        padding: 1rem;
-        margin: 1rem 0;
-        font-size: 2rem;
-        gap: .5rem;
-        span{
-            font-size: 2.5rem;
-            font-weight: 800;
-            color: var(--color-green);
-        }
-    }
-    .income-content{
-        display: flex;
-        gap: 2rem;
-        .incomes{
-            flex: 1;
-        }
-    }
-`;
 
 export default Expenses
